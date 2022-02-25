6ffe39e4b24f31e2b7379be665deccc06b377ec8
Karin <karindeoliveira@ibm.com>
2022-02-25 08:03:23 -0300

    deteccao de faces

--- src/main/java/Captura.java
+++ src/main/java/Captura.java
@@ -3,8 +3,18 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.objdetect.CascadeClassifier;
 
 import java.awt.event.KeyEvent;
+
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.opencv.imgproc.Imgproc.COLOR_BGRA2GRAY;
+import static org.opencv.imgproc.Imgproc.rectangle;
 
 public class Captura {
     public static void main(String arg[]) throws FrameGrabber.Exception {
@@ -17,11 +27,30 @@ public class Captura {
         //para tirar a foto
         camera.start();
 
+        //inserindo o arquivo que vai fazer a deteccao de faces
+        CascadeClassifier detectorFace = new CascadeClassifier("src\\resoueces\\haarcascade_frontalface_alt.xml");
+
         // janela de preview ao inves de utilizar este codigo de gamma pode ser 1 ou nada
         CanvasFrame cFrame = new CanvasFrame ("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
         Frame frameCapturado = null;
+        //por meio desta imagemColorida que irá ser feito a deteccao do frame
+        Mat imagemColorida = new Mat();
+
         //Se a camera estiver capturando algo o código vai ser diferente de null e vai executar o codigo abaixo
         while ((frameCapturado = camera.grab()) != null){
+            imagemColorida = converteMat.convert(frameCapturado);
+            Mat imagemCinza = new Mat();
+            //passar a imagem para a escala de cinza para o programa trabalhar melhor
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+            //armazena as faces detectadas nesta variavel
+            RectVector facesDetectadas = new RectVector();
+            //Passa a foto para faces detectadas e os outros são parametros de tamanho da face que pode ser detectada minimo e maximo
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150,150), new Size(500,500));
+            for (int i=0; i< facesDetectadas.size(); i++){
+                //fazer um retangulo por isso o rect
+                Rect dadosFace = facesDetectadas.get(0);
+                rectangle(imagemColorida, dadosFace, new Scalar(0,0,255,0));
+            }
             if (cFrame.isVisible()) {
                 cFrame.showImage(frameCapturado);
             }
