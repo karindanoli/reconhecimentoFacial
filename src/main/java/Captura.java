f9ec1d33d20b2a73edfaf0912bb2ea3218011fec
Karin Pinheiro <karinpinheiro.coach@gmail.com>
2022-03-05 21:57:47 -0300

    dependencias pom camera

--- src/main/java/Captura.java
+++ src/main/java/Captura.java
@@ -1,23 +1,36 @@
+import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Rect;
 import org.bytedeco.opencv.opencv_core.RectVector;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.objdetect.CascadeClassifier;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import static org.bytedeco.opencv.global.opencv_imgproc.resize;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+
+
 
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 
-import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
-import static org.opencv.imgproc.Imgproc.COLOR_BGRA2GRAY;
-import static org.opencv.imgproc.Imgproc.rectangle;
 
 public class Captura {
-    public static void main(String arg[]) throws FrameGrabber.Exception {
+    public static void main(String arg[]) throws IOException, InterruptedException {
+        try {
+            Loader.load(opencv_core.class);
+        } catch (UnsatisfiedLinkError e) {
+            String path = Loader.cacheResource(opencv_core.class, "windows-x86_64/jniopencv_core.dll").getPath();
+            new ProcessBuilder("C:/Users/PC/Desktop/projetos/dependenciesGUI", path).start().waitFor();
+        }
         //eventos do teclado
         KeyEvent tecla = null;
         //converte a imagem para o formato de matriz
@@ -27,33 +40,20 @@ public class Captura {
         //para tirar a foto
         camera.start();
 
-        //inserindo o arquivo que vai fazer a deteccao de faces
-        CascadeClassifier detectorFace = new CascadeClassifier("src\\resoueces\\haarcascade_frontalface_alt.xml");
+        //LINHA 1
 
         // janela de preview ao inves de utilizar este codigo de gamma pode ser 1 ou nada
         CanvasFrame cFrame = new CanvasFrame ("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
         Frame frameCapturado = null;
         //por meio desta imagemColorida que irá ser feito a deteccao do frame
-        Mat imagemColorida = new Mat();
+
 
         //Se a camera estiver capturando algo o código vai ser diferente de null e vai executar o codigo abaixo
         while ((frameCapturado = camera.grab()) != null){
-            imagemColorida = converteMat.convert(frameCapturado);
-            Mat imagemCinza = new Mat();
-            //passar a imagem para a escala de cinza para o programa trabalhar melhor
-            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
-            //armazena as faces detectadas nesta variavel
-            RectVector facesDetectadas = new RectVector();
-            //Passa a foto para faces detectadas e os outros são parametros de tamanho da face que pode ser detectada minimo e maximo
-            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150,150), new Size(500,500));
-            for (int i=0; i< facesDetectadas.size(); i++){
-                //fazer um retangulo por isso o rect
-                Rect dadosFace = facesDetectadas.get(0);
-                rectangle(imagemColorida, dadosFace, new Scalar(0,0,255,0));
-            }
-            if (cFrame.isVisible()) {
+            if (cFrame.isVisible()){
                 cFrame.showImage(frameCapturado);
-            }
+
+                }
             }
         cFrame.dispose();
         camera.stop();
